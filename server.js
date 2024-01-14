7a68f4561d92ae69c266cbd070356b30a2302ddf
Nazarenko <dasamant@ukr.net>
2024-01-14 19:44:23 +0200

    creatr server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+
+await mongoose
+  .connect("mongodb+srv://dasamant:12345@cluster0.k36mt3v.mongodb.net/")
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
