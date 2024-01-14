5a0f1ec16233b041912dcbdc20f7e5c0971e7bcf
Nazarenko <dasamant@ukr.net>
2024-01-14 20:09:24 +0200

    registration and login

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./backend/routes/userR.js";
 
 await mongoose
   .connect("mongodb+srv://dasamant:12345@cluster0.k36mt3v.mongodb.net/")
@@ -10,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
 });
+
+app.use("/auth", userRoute);
